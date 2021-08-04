514165ab96e8a7025d78c0c64b7320fd6221e6ff
Dipali Thakur <dipalithakur2001@gmail.com>
2021-08-04 20:28:22 +0530

    commit js

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const select = document.querySelectorAll('select');
+const input = document.querySelectorAll('input');
+const API_KEY ="http://api.exchangeratesapi.io/v1/latest?access_key=c23ffa411bb28c19e9aa0b8cb95c70a0 ";
+let html = '';
+
+async function currency(){
+    const res = await fetch(API_KEY);
+    const data = await res.json();
+    const arrKeys = Object.keys(data.rates);
+    const rates = data.rates;
+
+    arrKeys.map(item =>{
+        return html += `<option value=${item}>${item}</option>`;
+    });
+   
+    for(let i=0;i<select.length;i++){
+        select[i].innerHTML=html;
+    };
+     
+    function convert(i,j){
+        input[i].value = input[j].value * rates[select[i].value] /rates[select[j].value] ;
+    }
+   
+    input[0].addEventListener('keyup', ()=> convert(1,0))
+       
+
+    input[1].addEventListener('keyup', ()=> convert(0,1))
+      
+
+    select[0].addEventListener('change', ()=> convert(1,0))
+        
+
+    select[1].addEventListener('change', ()=> convert(0,1))
+       
+
+     
+
+};
+currency();
