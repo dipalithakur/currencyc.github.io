73c782dc0b2a8d6abefb6cb8c67d1c6678c1092d
viren-vii <virubhosale112@gmail.com>
2021-08-05 11:29:59 +0530

    Uing different api!

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
 
 const select = document.querySelectorAll('select');
 const input = document.querySelectorAll('input');
-const API_KEY ="http://api.exchangeratesapi.io/v1/latest?access_key=c23ffa411bb28c19e9aa0b8cb95c70a0";
+// const API_KEY ="http://api.exchangeratesapi.io/v1/latest?access_key=c23ffa411bb28c19e9aa0b8cb95c70a0";
 
 // gh http://api.exchangeratesapi.io/v1/latest?access_key=c23ffa411bb28c19e9aa0b8cb95c70a0
 
 let html = '';
-
 async function currency(){
-    const res = await fetch(API_KEY);
-    const data = await res.json();
-    console.log(data);
-    const arrKeys = Object.keys(data.rates);
-    const rates = data.rates;
+    // const res = await fetch(API_KEY);
+    // const data = await res.json();
 
-    arrKeys.map(item =>{
-        return html += `<option value=${item}>${item}</option>`;
-    });
-   
-    for(let i=0;i<select.length;i++){
-        select[i].innerHTML=html;
-    };
-     
-    function convert(i,j){
-        input[i].value = input[j].value * rates[select[i].value] /rates[select[j].value] ;
+    var requestURL = 'https://api.exchangerate.host/latest';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        data = request.response;
+        console.log(data);
+        const arrKeys = Object.keys(data.rates);
+        const rates = data.rates;
+        arrKeys.map(item =>{
+            return html += `<option value=${item}>${item}</option>`;
+        });
+       
+        for(let i=0;i<select.length;i++){
+            select[i].innerHTML=html;
+        };
+         
+        function convert(i,j){
+            input[i].value = input[j].value * rates[select[i].value] /rates[select[j].value] ;
+        }
+       
+        input[0].addEventListener('keyup', ()=> convert(1,0))
+           
+    
+        input[1].addEventListener('keyup', ()=> convert(0,1))
+          
+    
+        select[0].addEventListener('change', ()=> convert(1,0))
+            
+    
+        select[1].addEventListener('change', ()=> convert(0,1))
     }
-   
-    input[0].addEventListener('keyup', ()=> convert(1,0))
-       
-
-    input[1].addEventListener('keyup', ()=> convert(0,1))
-      
-
-    select[0].addEventListener('change', ()=> convert(1,0))
-        
-
-    select[1].addEventListener('change', ()=> convert(0,1))
-       
-
-     
 
 };
 currency();
