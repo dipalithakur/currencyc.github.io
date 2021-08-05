979b6906c81f9d5b9391456cab8bdd270c7db10a
viren-vii <virubhosale112@gmail.com>
2021-08-05 11:32:48 +0530

    Cleanup

--- app.js
+++ app.js
@@ -28,20 +28,17 @@ async function currency(){
         for(let i=0;i<select.length;i++){
             select[i].innerHTML=html;
         };
-         
+        
         function convert(i,j){
             input[i].value = input[j].value * rates[select[i].value] /rates[select[j].value] ;
         }
        
-        input[0].addEventListener('keyup', ()=> convert(1,0))
-           
+        input[0].addEventListener('keyup', ()=> convert(1,0))    
     
         input[1].addEventListener('keyup', ()=> convert(0,1))
           
-    
         select[0].addEventListener('change', ()=> convert(1,0))
             
-    
         select[1].addEventListener('change', ()=> convert(0,1))
     }
 
